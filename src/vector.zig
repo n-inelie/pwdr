fbabd916f903893bcdeec26af4afbda335df8502
n-inelie <nin3li3@yahoo.com>
2024-07-19 01:02:10 +1000

    vector got vectored

--- src/vector.zig
+++ src/vector.zig
@@ -38,10 +38,26 @@ pub fn Vector(comptime T: type) type {
     };
 }
 
+pub inline fn Vector2i(x: i32, y: i32) Vector(i32) {
+    return Vector(f32).Create(&.{ x, y });
+}
+
+pub inline fn Vector3i(x: i32, y: i32, z: i32) Vector(i32) {
+    return Vector(i32).Create(&.{ x, y, z });
+}
+
+pub inline fn Vector4i(x: i32, y: i32, z: i32, w: i32) Vector(i32) {
+    return Vector(i32).Create(&.{ x, y, z, w });
+}
+
 pub inline fn Vector2f(x: f32, y: f32) Vector(f32) {
     return Vector(f32).Create(&.{ x, y });
 }
 
 pub inline fn Vector3f(x: f32, y: f32, z: f32) Vector(f32) {
     return Vector(f32).Create(&.{ x, y, z });
+}
+
+pub inline fn Vector4f(x: f32, y: f32, z: f32, w: f32) Vector(f32) {
+    return Vector(f32).Create(&.{ x, y, z, w });
 }

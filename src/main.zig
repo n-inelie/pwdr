5311afc5abe6d4cae6ab1337765a537bfaa8707d
n-inelie <nin3li3@yahoo.com>
2024-07-19 00:26:34 +1000

    add syntactic sugar to init vectors

--- src/main.zig
+++ src/main.zig
@@ -7,21 +7,21 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    var m1 = try matrix.Matrix(i8).init(gpa.allocator(), 3, 3);
-    defer m1.deinit();
-    try m1.makeScaler(3);
-    try m1.lazyPrint();
+    // var m1 = try matrix.Matrix(i8).init(gpa.allocator(), 3, 3);
+    // defer m1.deinit();
+    // try m1.makeScaler(3);
+    // try m1.lazyPrint();
 
-    var m2 = try matrix.Matrix(i8).init(gpa.allocator(), 3, 3);
-    defer m2.deinit();
-    try m2.makeScaler(3);
-    try m2.lazyPrint();
+    // var m2 = try matrix.Matrix(i8).init(gpa.allocator(), 3, 3);
+    // defer m2.deinit();
+    // try m2.makeScaler(3);
+    // try m2.lazyPrint();
 
-    var m3 = try matrix.Multiply(i8, gpa.allocator(), m1, m2);
-    defer m3.deinit();
+    // var m3 = try matrix.Multiply(i8, gpa.allocator(), m1, m2);
+    // defer m3.deinit();
+    // try m3.lazyPrint();
 
-    try m3.lazyPrint();
-
-    // const v1 = vector.Vector2(u8).init(3, 4);
-    // print("{}, {}\n", .{ v1.magnitudeSquared(), v1.magnitude() });
+    var v1 = try vector.Vector(i8).init(gpa.allocator(), &.{ 3, 4 });
+    defer v1.deinit();
+    print("{}, {}\n", .{ v1.magnitudeSquared(), v1.magnitude() });
 }
--- src/vector.zig
+++ src/vector.zig
@@ -1,20 +1,47 @@
 const std = @import("std");
 const math = std.math;
 
-pub fn Vector(comptime T: type, comptime Dimensions: usize) type {
+pub fn Vector(comptime T: type) type {
     return struct {
         const Self = @This();
 
-        items: std.ArrayList(T),
+        elements: std.ArrayList(T),
+        dimensions: usize,
 
-        pub fn init(allocator: std.mem.Allocator) Self {
-            var items = std.ArrayList(T).init(allocator);
-            items.appendNTimes(0, Dimensions);
-            return .{ .items = items };
+        pub fn init(allocator: std.mem.Allocator, args: []const T) !Self {
+            var elements = std.ArrayList(T).init(allocator);
+            for (args) |arg| {
+                try elements.append(arg);
+            }
+            return .{
+                .elements = elements,
+                .dimensions = elements.items.len,
+            };
         }
 
         pub fn deinit(self: *Self) void {
-            self.items.deinit();
+            self.elements.deinit();
+        }
+
+        pub fn magnitudeSquared(self: Self) f32 {
+            var magnitude_squared: f32 = 0;
+            switch (@typeInfo(T)) {
+                .Int => {
+                    for (self.elements.items) |element| {
+                        magnitude_squared += @floatFromInt(math.pow(T, element, 2));
+                    }
+                },
+                else => {
+                    for (self.elements.items) |element| {
+                        magnitude_squared += @floatCast(math.pow(T, element, 2));
+                    }
+                },
+            }
+            return magnitude_squared;
+        }
+
+        pub fn magnitude(self: Self) f32 {
+            return math.sqrt(self.magnitudeSquared());
         }
     };
 }

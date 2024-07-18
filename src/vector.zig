e1953e72bc6ff55e4dc5763e233abbadebbc969f
n-inelie <nin3li3@yahoo.com>
2024-07-18 21:24:16 +1000

    fix that damn vector!!!

--- src/vector.zig
+++ src/vector.zig
@@ -1,34 +1,20 @@
 const std = @import("std");
 const math = std.math;
 
-pub fn Vector2(comptime T: type) type {
+pub fn Vector(comptime T: type, comptime Dimensions: usize) type {
     return struct {
         const Self = @This();
 
-        x: T,
-        y: T,
+        items: std.ArrayList(T),
 
-        pub inline fn init(
-            x: T,
-            y: T,
-        ) Self {
-            return .{
-                .x = x,
-                .y = y,
-            };
+        pub fn init(allocator: std.mem.Allocator) Self {
+            var items = std.ArrayList(T).init(allocator);
+            items.appendNTimes(0, Dimensions);
+            return .{ .items = items };
         }
 
-        pub fn magnitudeSquared(self: Self) T {
-            return math.pow(T, self.x, 2) + math.pow(T, self.y, 2);
-        }
-
-        pub fn magnitude(self: Self) f32 {
-            var m: f32 = undefined;
-            switch (@typeInfo(T)) {
-                .Int => m = @floatFromInt(self.magnitudeSquared()),
-                else => m = @floatCast(self.magnitudeSquared()),
-            }
-            return math.sqrt(m);
+        pub fn deinit(self: *Self) void {
+            self.items.deinit();
         }
     };
 }
